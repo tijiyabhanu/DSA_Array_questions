021d31edd59f18da72cd7b16ab0076c65ec088a4
Tijiya_bhanu <23kq1a5487@pace.ac.in>
2026-03-20 13:16:20 +0530

    Create check_if_the_array_is_sorted.py

---
+++ check_if_the_array_is_sorted.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isSorted(self, arr) -> bool:
+        # code here
+        for i in range(len(arr)-1):
+            if arr[i]>arr[i+1]:
+                return False
+        return True
